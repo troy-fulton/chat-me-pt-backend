2560fafc8c886702fe716176eb796947d08ba2f2
Troy Fulton <25339228+troy-fulton@users.noreply.github.com>
2025-06-15 15:51:54 -0500

    added CORS support

--- chat_me_pt_backend/settings.py
+++ chat_me_pt_backend/settings.py
@@ -19,11 +19,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "api",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -82,3 +84,13 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+
+if IN_RAILWAY:
+    FRONTEND_URL = os.getenv("FRONTEND_URL", "")
+    CORS_ALLOWED_ORIGINS = [
+        FRONTEND_URL,
+    ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^http://localhost:\d+$",
+    ]
